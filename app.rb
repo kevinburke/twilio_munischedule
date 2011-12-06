ac26c43f4a886cb967a15e5bace5a1dba6b2a8ae
Andrew Jordan <andrew.k.jordan@gmail.com>
2011-12-06 14:59:41 -0800

    simple heroku twilio app to start

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'sinatra'
+require 'muni'
+require 'builder'
+
+get '/' do
+  builder do |xml|
+    xml.instruct!
+    xml.Response do 
+      xml.Say("Its a work in progress")
+    end
+  end
+ #r45 = Muni::Route.find(45)
+ #r45.outbound.stop_at("Union and Leavenworth").predictions
+end
+
+#16769
