857329302bf3f4a7144cc38072131366938b0f5b
Andrew Jordan <andrew.k.jordan@gmail.com>
2011-12-19 14:45:29 -0800

    Maybe the whole thing works, but probably it's just broken

--- app.rb
+++ app.rb
@@ -1,16 +1,38 @@
-require 'rubygems'
 require 'sinatra'
-require 'builder'
+require 'muni'
+require 'twilio-ruby'
 
-post '/' do
-  builder do |xml|
-    xml.instruct!
-    xml.Response do 
-      xml.Say("Its a work in progress")
+get '/' do
+  response = Twilio::TwiML::Response.new do |r|
+    r.Gather :action => "input", :numDigits => 1 do
+      r.Say "Hello, are you headed to of back from work"
+      r.Say "Press 1 for inbound"
+      r.Say "press 2 for outbound"
     end
   end
- #r45 = Muni::Route.find(45)
- #r45.outbound.stop_at("Union and Leavenworth").predictions
+  response.text
 end
 
-#16769
+post '/input' do
+  user_input = params[:Digits]
+  
+  if user_input == "1"
+    bus = Muni::Route.find(user_input)
+    a = bus.inbound.stop_at("Union and Leavenworth").predictions
+    arriving_in = {:minutes => a.first.minutes, :seconds => a.first.seconds}
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "Hey Andrew the 45 inbound will arrive in #{arriving_in[:minutes]} minutes and #{arriving_in[:seconds]} seconds"
+    end
+    response.text
+    
+  elsif user_input == "2"
+    bus = Muni::Route.find(user_input)
+    a = bus.outbound.stop_at("Union and Leavenworth").predictions
+    arriving_in = {:minutes => a.first.minutes, :seconds => a.first.seconds}
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "Hey Andrew the 45 outbound will arrive in #{arriving_in[:minutes]} minutes and #{arriving_in[:seconds]} seconds"
+    end
+    response.text
+  end
+  
+end
