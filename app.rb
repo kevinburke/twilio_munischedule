7993a6dad27646784f1ae58ab84bea528863fcc9
Andrew Jordan <andrew.k.jordan@gmail.com>
2011-12-19 15:43:04 -0800

    option 2 works now as well

--- app.rb
+++ app.rb
@@ -5,8 +5,8 @@ require 'twilio-ruby'
 get '/' do
   response = Twilio::TwiML::Response.new do |r|
     r.Gather :action => "input", :numDigits => 1 do
-      r.Say "Hello Andrew are headed to Twilio... or headed home..."
-      r.Say "Press 1 for Twilio..."
+      r.Say "Hello Andrew, are headed to Twilio, or headed home,"
+      r.Say "Press 1 for Twilio,"
       r.Say "Press 2 for home"
     end
   end
@@ -21,20 +21,24 @@ post '/input' do
     a = bus.inbound.stop_at("Union and Leavenworth").predictions
     arriving_in = {:first => a.first.minutes, :second => a.second.minutes,:third => a.third.minutes,}
     bustwo = Muni::Route.find(12)
-    b = bustwo.inbound.stop_at("Pacific and Leavenworth").predictions
+    b = bustwo.outbound.stop_at("Pacific and Leavenworth").predictions
     arriving_in_2 = {:first => b.first.minutes, :second => b.second.minutes,:third => b.third.minutes,}
     response = Twilio::TwiML::Response.new do |r|
       r.Say "The 45 will depart in #{arriving_in[:first]}, #{arriving_in[:second]}, and #{arriving_in[:third]} minutes"
-      r.Say "The 21 will depart in #{arriving_in_2[:first]}, #{arriving_in_2[:second]}, and #{arriving_in_2[:third]} minutes"
+      r.Say "The 12 will depart in #{arriving_in_2[:first]}, #{arriving_in_2[:second]}, and #{arriving_in_2[:third]} minutes"
     end
     response.text
     
   elsif user_input == "2"
-    bus = Muni::Route.find(user_input)
-    a = bus.outbound.stop_at("Union and Leavenworth").predictions
-    arriving_in = {:minutes => a.first.minutes, :seconds => a.first.seconds}
+    bus = Muni::Route.find(45)
+    a = bus.outbound.stop_at("3rd and Folsom").predictions
+    arriving_in = {:first => a.first.minutes, :second => a.second.minutes,:third => a.third.minutes,}
+    bustwo = Muni::Route.find(12)
+    b = bustwo.inbound.stop_at("2nd and Howard").predictions
+    arriving_in_2 = {:first => b.first.minutes, :second => b.second.minutes,:third => b.third.minutes,}
     response = Twilio::TwiML::Response.new do |r|
-      r.Say "Hey Andrew the 45 outbound will arrive in #{arriving_in[:minutes]} minutes and #{arriving_in[:seconds]} seconds"
+      r.Say "The 45 will depart in #{arriving_in[:first]}, #{arriving_in[:second]}, and #{arriving_in[:third]} minutes"
+      r.Say "The 12 will depart in #{arriving_in_2[:first]}, #{arriving_in_2[:second]}, and #{arriving_in_2[:third]} minutes"
     end
     response.text
   end
