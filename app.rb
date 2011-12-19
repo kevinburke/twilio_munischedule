29aed7722169ecdef04fdc40965ad4e72f722521
Andrew Jordan <andrew.k.jordan@gmail.com>
2011-12-19 15:20:11 -0800

    update with two routes per menu option

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ require 'twilio-ruby'
 get '/' do
   response = Twilio::TwiML::Response.new do |r|
     r.Gather :action => "input", :numDigits => 1 do
-      r.Say "Hello Andrew are you inbound to or outbound from Twilio..."
-      r.Say "Press 1 for inbound..."
-      r.Say "Press 2 for outbound"
+      r.Say "Hello Andrew are headed to Twilio... or headed home..."
+      r.Say "Press 1 for Twilio..."
+      r.Say "Press 2 for home"
     end
   end
   response.text
@@ -17,11 +17,15 @@ post '/input' do
   user_input = params[:Digits]
   
   if user_input == "1"
-    bus = Muni::Route.find(user_input)
+    bus = Muni::Route.find(45)
     a = bus.inbound.stop_at("Union and Leavenworth").predictions
-    arriving_in = {:minutes => a.first.minutes, :seconds => a.first.seconds}
+    arriving_in = {:first => a.first.minutes, :second => a.second.minutes,:third => a.third.minutes,}
+    bustwo = Muni::Route.find(12)
+    b = bustwo.inbound.stop_at("Pacific and Leavenworth").predictions
+    arriving_in_2 = {:first => b.first.minutes, :second => b.second.minutes,:third => b.third.minutes,}
     response = Twilio::TwiML::Response.new do |r|
-      r.Say "Hey Andrew the 45 inbound will arrive in #{arriving_in[:minutes]} minutes and #{arriving_in[:seconds]} seconds"
+      r.Say "The 45 will depart in #{arriving_in[:first]}, #{arriving_in[:second]}, and #{arriving_in[:third]} minutes"
+      r.Say "The 21 will depart in #{arriving_in_2[:first]}, #{arriving_in_2[:second]}, and #{arriving_in_2[:third]} minutes"
     end
     response.text
     
